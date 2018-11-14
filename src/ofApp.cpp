f58322d0123b42bd18895d06225a334d441e1e04
Compwork19 <Compwork19@user.noreply.github.com>
2018-11-14 15:56:11 -0400

    Added file

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,10 +1,25 @@
 #include "ofApp.h"
+#include <random>
 
 //--------------------------------------------------------------
 void ofApp::setup(){
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+
+std::random_device rd;
+std::mt19937 eng(rd());
+std::uniform_int_distribution<> un(30,1000);
+std::uniform_int_distribution<> un1(0,630);
+
+for(int i=0;i<20;i++)
+ cout<<un(eng)<<endl;
+ 
+ cout<<endl;
+ 
+ for(int i=0;i<20;i++)
+ cout<<un1(eng)<<endl;
+
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+//Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
 }
 
 //--------------------------------------------------------------
